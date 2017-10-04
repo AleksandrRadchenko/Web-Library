857a4f91df277f9218a5f046b7a8196f7438894b
Taras Moisiuk <t.v.moisiuk@gmail.com>
2017-10-05 00:07:47 +0400

    EPMWL-4.1 implement OrderDAO

--- src/main/java/com/epam/wl/dao/OrderDAO.java
+++ src/main/java/com/epam/wl/dao/OrderDAO.java
@@ -1,4 +1,20 @@
 package com.epam.wl.dao;
 
-public class OrderDAO {
+import com.epam.wl.entities.Book;
+import com.epam.wl.entities.Order;
+import com.epam.wl.entities.User;
+
+import java.util.List;
+
+public interface OrderDAO {
+    void create(User user, Book book);
+
+    void setInProgress(int orderID);
+
+    void close(int orderID);
+
+    List<Order> getAll();
+
+    List<Order> getNew();
+
 }
