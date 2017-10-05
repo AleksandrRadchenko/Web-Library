6f3e771a59166f177e9c4a74e11dd3b690a72921
kshavonina <kshavonina@yandex.ru>
2017-10-05 19:51:48 +0300

    EPMWL-7 Delete comments

--- src/main/java/com/epam/wl/executor/Executor.java
+++ src/main/java/com/epam/wl/executor/Executor.java
@@ -5,13 +5,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-/**
- * @author v.chibrikov
- *         <p>
- *         Пример кода для курса на https://stepic.org/
- *         <p>
- *         Описание курса и лицензия: https://github.com/vitaly-chibrikov/stepic_java_webserver
- */
 public class Executor {
     private final Connection connection;
 
@@ -25,9 +18,7 @@ public class Executor {
         stmt.close();
     }
 
-    public <T> T execQuery(String query,
-                           ResultHandler<T> handler)
-            throws SQLException {
+    public <T> T execQuery(String query, ResultHandler<T> handler) throws SQLException {
         Statement stmt = connection.createStatement();
         stmt.execute(query);
         ResultSet result = stmt.getResultSet();
