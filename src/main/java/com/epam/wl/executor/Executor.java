de6154d3bb07bacce96adbb321111a85ea4f065c
kshavonina <kshavonina@yandex.ru>
2017-10-06 13:28:53 +0300

    EPMWL-7 Make additional amendments in accordance with the Vlad's comments

--- src/main/java/com/epam/wl/executor/Executor.java
+++ src/main/java/com/epam/wl/executor/Executor.java
@@ -12,13 +12,13 @@ public class Executor {
         this.connection = connection;
     }
 
-    public void executeUpdate(String update) throws SQLException {
+    public void executeUpdate(final String update) throws SQLException {
         final Statement stmt = connection.createStatement();
         stmt.execute(update);
         stmt.close();
     }
 
-    public <T> T executeQuery(String query, ResultHandler<T> handler) throws SQLException {
+    public <T> T executeQuery(final String query, final ResultHandler<T> handler) throws SQLException {
         final Statement stmt = connection.createStatement();
         stmt.execute(query);
         final ResultSet result = stmt.getResultSet();
--- src/main/java/com/epam/wl/executor/ResultHandler.java
+++ src/main/java/com/epam/wl/executor/ResultHandler.java
@@ -4,5 +4,5 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public interface ResultHandler<T> {
-    T handle(ResultSet resultSet) throws SQLException;
+    T handle(final ResultSet resultSet) throws SQLException;
 }
