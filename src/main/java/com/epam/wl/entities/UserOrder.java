6243cf0575da608d56d55ea5ff4f86542377e70d
Alexander Radchenko <partner.ar@gmail.com>
2017-10-07 21:26:10 +0300

    EPMWL-6 Add entities
    Rename Order->UserOrder

--- src/main/java/com/epam/wl/entities/UserOrder.java
+++ src/main/java/com/epam/wl/entities/UserOrder.java
@@ -9,11 +9,4 @@ public class UserOrder {
     private int editionId;
     private int userId;
     private OrderStatus status;
-
-    public UserOrder(int id, int editionId, int userId, OrderStatus status) {
-        this.id = id;
-        this.editionId = editionId;
-        this.userId = userId;
-        this.status = status;
-    }
 }
