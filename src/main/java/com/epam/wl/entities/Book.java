5dc8a416dbbe81745fe8d530e0a5a837be0e5ac0
kshavonina <kshavonina@yandex.ru>
2017-10-08 02:04:27 +0300

    Merge branches 'TASK-3.1-Implement-BookDAO' and 'dev' of https://github.com/Alsturm/epm-wl into TASK-3.1-Implement-BookDAO
    # Conflicts:
    #	gradle/wrapper/gradle-wrapper.properties
    #	src/main/java/com/epam/wl/entities/BookInstance.java

--- src/main/java/com/epam/wl/entities/Book.java
+++ src/main/java/com/epam/wl/entities/Book.java
@@ -10,13 +10,6 @@ public class Book {
     private String title;
     private int year;
 
-    public Book(int id, String author, String title, int year) {
-        this.id = id;
-        this.author = author;
-        this.title = title;
-        this.year = year;
-    }
-
     public static void main(String[] args) {
         Book book = new Book(4, "Лев Толстой", "Война и мир", 1978);
         System.out.println(book.getAuthor());
