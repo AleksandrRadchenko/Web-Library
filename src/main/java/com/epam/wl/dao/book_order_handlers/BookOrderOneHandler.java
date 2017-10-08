b1cd274b88dbc96f629b6d6aa1da4e930ff23405
Alexander Radchenko <partner.ar@gmail.com>
2017-10-08 18:40:29 +0300

    EPMWL-5.1 Make handler return Optional

--- src/main/java/com/epam/wl/dao/book_order_handlers/BookOrderOneHandler.java
+++ src/main/java/com/epam/wl/dao/book_order_handlers/BookOrderOneHandler.java
@@ -6,16 +6,17 @@ import com.epam.wl.executor.ResultHandler;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Optional;
 
-public class BookOrderOneHandler implements ResultHandler<BookOrder> {
+public class BookOrderOneHandler implements ResultHandler<Optional<BookOrder>> {
     @Override
-    public BookOrder handle(ResultSet resultSet) throws SQLException {
-        resultSet.next();
+    public Optional<BookOrder> handle(ResultSet resultSet) throws SQLException {
+        if (!resultSet.next()) return Optional.empty();
         int id = resultSet.getInt("id");
         int book_instanceid = resultSet.getInt("book_instanceid");
         int user_orderid = resultSet.getInt("user_orderid");
         BookOptions option = BookOptions.valueOf(resultSet.getString("option"));
-        return new BookOrder(id, book_instanceid, user_orderid, option);
+        return Optional.of(new BookOrder(id, book_instanceid, user_orderid, option));
     }
 }
 
