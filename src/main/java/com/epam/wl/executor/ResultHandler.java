9589cef561f264bae24c7d83be771bbfa9246c90
Alexander Radchenko <partner.ar@gmail.com>
2017-10-08 17:37:58 +0300

    EPMWL-6.3 ResultHandler -> functional interface
    (cherry picked from commit 0930941)

--- src/main/java/com/epam/wl/executor/ResultHandler.java
+++ src/main/java/com/epam/wl/executor/ResultHandler.java
@@ -3,6 +3,7 @@ package com.epam.wl.executor;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+@FunctionalInterface
 public interface ResultHandler<T> {
     T handle(final ResultSet resultSet) throws SQLException;
 }
