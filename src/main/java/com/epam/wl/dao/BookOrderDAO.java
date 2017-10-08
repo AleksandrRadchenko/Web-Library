8d218570496325e6d0b2554866d25ced07d8aeb9
Alexander Radchenko <partner.ar@gmail.com>
2017-10-08 17:27:14 +0300

    EPMWL-5.1 DAO.create() runs via executor

--- src/main/java/com/epam/wl/dao/BookOrderDAO.java
+++ src/main/java/com/epam/wl/dao/BookOrderDAO.java
@@ -1,73 +1,51 @@
 package com.epam.wl.dao;
 
-import com.epam.wl.dao.book_order_handlers.*;
+import com.epam.wl.dao.book_order_handlers.BookOrderListHandler;
+import com.epam.wl.dao.book_order_handlers.BookOrderOneHandler;
 import com.epam.wl.entities.BookInstance;
 import com.epam.wl.entities.BookOrder;
 import com.epam.wl.entities.UserOrder;
 import com.epam.wl.enums.BookOptions;
 import com.epam.wl.executor.Executor;
 import com.epam.wl.executor.ResultHandler;
-import lombok.RequiredArgsConstructor;
 
 import javax.sql.DataSource;
-import java.sql.*;
+import java.sql.SQLException;
 import java.util.List;
 
-@RequiredArgsConstructor
 public class BookOrderDAO {
     private final DataSource dataSource;
+    private Executor executor;
     private final ResultHandler<BookOrder> bookOrderOneHandler = new BookOrderOneHandler();
     private final ResultHandler<List<BookOrder>> bookOrderListHandler = new BookOrderListHandler();
 
+    public BookOrderDAO(DataSource dataSource) {
+        this.dataSource = dataSource;
+        executor = new Executor(dataSource);
+        ;
+    }
 
     /**
-     * Create row and returns 1 (number of rows changed)
+     * Create row and returns 1 (number of rows changed) or throws SQLException
      */
-    int create(final BookInstance bookInstance, final UserOrder userOrder, final BookOptions bookOption) {
-        try (Connection connection = dataSource.getConnection()){
-            PreparedStatement preparedStatement = connection.prepareStatement(
-                    "INSERT INTO book_order (book_instanceid, user_orderid, option) VALUES(?, ?, ?);");
-            preparedStatement.setInt(1, bookInstance.getId());
-            preparedStatement.setInt(2, userOrder.getId());
-            preparedStatement.setString(3, bookOption.toString());
-            preparedStatement.executeUpdate();
-            return 1;
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return 0; // In case of exception, no rows was changed
+    int create(
+            final BookInstance bookInstance,
+            final UserOrder userOrder,
+            final BookOptions bookOption)
+            throws SQLException {
+        String updateQuery = "INSERT INTO book_order (book_instanceid, user_orderid, option) VALUES(%d, %d, '%s');";
+        executor.executeUpdate(String.format(updateQuery, bookInstance.getId(), userOrder.getId(), bookOption.toString()));
+        return 1;
     }
 
 
     public List<BookOrder> getAll() throws SQLException {
-        try (Connection connection = dataSource.getConnection()) {
-            Statement st = connection.createStatement();
-            st.executeQuery("SELECT id, book_instanceid, user_orderid, option FROM book_order");
-            BookOrderListHandler h = new BookOrderListHandler();
-            return h.handle(st.getResultSet());
-        }
+        final String query = "SELECT id, book_instanceid, user_orderid, option FROM book_order";
+        return executor.executeQuery(query, bookOrderListHandler);
     }
 
     public BookOrder getById(final int id) throws SQLException {
-        BookOrder result;
-        try (Connection connection = dataSource.getConnection()) {
-            Executor ex = new Executor(connection);
-            String query = "SELECT id, book_instanceid, user_orderid, option FROM book_order WHERE id=" + id;
-            return ex.executeQuery(query, bookOrderOneHandler);
-        }
-    }
-
-    static void printResultSet(ResultSet set) throws SQLException {
-        int row = 1;
-        while (set.next()) {
-            System.out.print(row++ + " : ");
-            ResultSetMetaData metaData = set.getMetaData();
-            for (int i = 1; i < metaData.getColumnCount(); i++) {
-                Object o = set.getObject(i);
-                System.out.print(metaData.getColumnName(i) + " = " + o.toString() + "; ");
-            }
-            int lastColumn = metaData.getColumnCount();
-            System.out.println(metaData.getColumnName(lastColumn) + " = " + set.getObject(lastColumn));
-        }
+        final String query = "SELECT id, book_instanceid, user_orderid, option FROM book_order WHERE id=" + id;
+        return executor.executeQuery(query, bookOrderOneHandler);
     }
 }
