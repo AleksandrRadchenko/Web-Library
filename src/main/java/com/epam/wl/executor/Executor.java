61178d8d723bfb098eb65bd1bfe38a12f0e40ebc
Alexander Radchenko <partner.ar@gmail.com>
2017-10-08 17:23:25 +0300

    EPMWL-5.1 Moved try() into Executor

--- src/main/java/com/epam/wl/executor/Executor.java
+++ src/main/java/com/epam/wl/executor/Executor.java
@@ -1,32 +1,29 @@
 package com.epam.wl.executor;
 
-import java.sql.Connection;
+import lombok.RequiredArgsConstructor;
+
+import javax.sql.DataSource;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+@RequiredArgsConstructor
 public class Executor {
-    private final Connection connection;
-
-    public Executor(Connection connection) {
-        this.connection = connection;
-    }
+    private final DataSource dataSource;
 
     public void executeUpdate(final String update) throws SQLException {
-        final Statement stmt = connection.createStatement();
-        stmt.execute(update);
-        stmt.close();
+        try (Statement stmt = dataSource.getConnection().createStatement()) {
+            stmt.execute(update);
+        }
     }
 
     public <T> T executeQuery(final String query, final ResultHandler<T> handler) throws SQLException {
-        final Statement stmt = connection.createStatement();
-        stmt.execute(query);
-        final ResultSet result = stmt.getResultSet();
-        T value = handler.handle(result);
-        result.close();
-        stmt.close();
-
-        return value;
+        try (Statement stmt = dataSource.getConnection().createStatement()) {
+            stmt.execute(query);
+            final ResultSet result = stmt.getResultSet();
+            T value = handler.handle(result);
+            result.close();
+            return value;
+        }
     }
-
 }
