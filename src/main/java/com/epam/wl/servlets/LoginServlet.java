34b43e3d0160ee71d574221d11dc810ae3786520
kshavonina <kshavonina@yandex.ru>
2017-10-14 20:23:39 +0300

    EPMWL-2 Move user confirmation process to the service

--- src/main/java/com/epam/wl/servlets/LoginServlet.java
+++ src/main/java/com/epam/wl/servlets/LoginServlet.java
@@ -1,16 +1,12 @@
 package com.epam.wl.servlets;
 
-import com.epam.wl.entities.User;
 import com.epam.wl.services.TestLoginService;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.sql.SQLException;
-import java.util.Optional;
 
 @javax.servlet.annotation.WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
@@ -21,22 +17,6 @@ public class LoginServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String email = request.getParameter("email");
-        String password = request.getParameter("password");
-
-        try {
-            Optional<User> userOptional = service.userDAO.getUserByEmailAndPassword(email, password);
-
-            if (userOptional.isPresent()) {
-                HttpSession session = request.getSession(true);
-                session.setAttribute("currentSessionUser", userOptional.get());
-
-                response.sendRedirect(service.getRolePage(email));
-            } else {
-                response.sendRedirect("login_error.jsp");
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        response.sendRedirect(service.confirmUser(request));
     }
 }
